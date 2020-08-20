1f84155f87d7a070742781b6016f9e60c7244702
Shivam Tailor <shivamtailor@gmail.com>
2020-08-20 16:28:08 -0400

    Customer Model : Add new "Email" field 1) Add new email field in migration script 2) Run php artisan migrate:rolback 3) Run php artisan migrate 4) Modify CustomerController to validate/save email field 5) Modify customers.blade.php to add email field 6) Move navigation html from layout.blade.php to nav.blade.php 7) Add section for "title" tag

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,7 @@
 @extends('layout')
 
+@section('title','Home')
+
 @section('content')
     <h1>Welcome to Laravel...</h1>
 @endsection
--- resources/views/internals/customers.blade.php
+++ resources/views/internals/customers.blade.php
@@ -1,20 +1,43 @@
 @extends('layout')
 
-@section('content')
-    <h1>Customers</h1>
+@section('title','Customer List')
 
-    <form action="customers" method="post" class="pb-5">
-        <div class="form-group">
-            <label for="name">Name</label>
-            <input type="text" class="form-control" id="name" name="name" placeholder="Name" aria-describedby="nameError">
-            <small id="nameError" class="form-text text-danger">{{ $errors->first('name') }}</small>
+@section('content')
+    <div class="row">
+        <div class="col-12">
+            <h1>Customers</h1>
         </div>
-        <button type="submit" class="btn btn-primary">Add Customer</button>
-        @csrf
-    </form>
-    <ul class="list-group">
-        @foreach($customersList as $customer)
-            <li class="list-group-item">{{$customer->name}}</li>
-        @endforeach
-    </ul>
+    </div>
+    <div class="row">
+        <div class="col-12">
+            <form action="customers" method="post" class="pb-5">
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input type="text" class="form-control" id="name" name="name" placeholder="Name" aria-describedby="nameError" value="{{old('name')}}">
+                    <small id="nameError" class="form-text text-danger">{{ $errors->first('name') }}</small>
+                </div>
+                <div class="form-group">
+                    <label for="name">Email</label>
+                    <input type="text" class="form-control" id="email" name="email" placeholder="Email" aria-describedby="emailError"  value="{{old('email')}}">
+                    <small id="emailError" class="form-text text-danger">{{ $errors->first('email') }}</small>
+                </div>
+                <button type="submit" class="btn btn-primary">Add Customer</button>
+                @csrf
+            </form>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12">
+            <ul class="list-group">
+                @foreach($customersList as $customer)
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                        {{$customer->name}}
+                        <span class="badge badge-primary badge-pill">{{$customer->email}}</span>
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+    </div>
+
+
 @endsection
